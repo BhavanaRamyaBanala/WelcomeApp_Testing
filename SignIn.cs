bc931bce1d0a207b7b49f8aa4682cb500c8174c9
BhavanaRamyaBanala <ramyabanala12@gmail.com>
2024-02-15 12:11:51 +0530

    Final Commit

---
+++ SignIn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+    public class SignIn
+    {
+        public string Authenticate(string userName, string password) {
+            string msg;
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                msg = "userName and password required!!";   
+            }
+            else {
+                if ((userName=="admin") && (password=="admin@1256")) {
+                    msg ="Authentication pass";
+                }
+                else
+                {
+                    msg = "Authentication fail";
+                }
+            }
+            return "Provide user details";
+        }
+    }
+}
